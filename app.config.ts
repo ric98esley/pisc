62152a8029e5b299214b9a20adf00c74869a1481
Ricardo Perez <ricardo.pereze.98@gmail.com>
2024-06-14 11:45:07 -0400

    gfdgdg

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,18 @@ export default defineAppConfig({
   ui: {
     primary: 'penn',
     gray: 'sealsalt',
-  }
-})
+    button: {
+      color: {
+        gray: {
+          solid: 'dark:text-gray-50',
+        },
+        black: {
+          solid: 'text-primary dark:text-gray-50',
+        },
+      },
+      variant: {
+        solid: 'dark:text-gray-50',
+      }
+    },
+  },
+});
